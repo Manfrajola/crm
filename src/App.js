57840b4847a6c9d0ae91f99a629ddef34cb9adf2
Jair Alves <112443175+Manfrajola@users.noreply.github.com>
2023-01-19 14:24:51 -0300

    EquipeC
    Criação de estilo para equipe C - 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./styles/GlogalStyle";
 import Home from "./pages/Home";
+import Equipec from "./pages/Equipec";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/equipec" element={<Equipec />} />
           </Routes>
         </div>
       </Router>
---
+++ src/pages/Equipec/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../../components/navbarUser";
+import {H1} from "./styles";
+
+class Equipec extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar />
+        <H1>Equipec</H1>
+      </>
+    );
+  }
+}
+
+export default Equipec;
