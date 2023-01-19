e8125a6e2d37b72fd813d0f8841f77f08cd5ca8f
Ricardo Rosendo <88691117+RicardoRosendo98@users.noreply.github.com>
2023-01-19 16:18:44 -0300

    equipej
    teste equipe

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import GlobalStyle from "./styles/GlogalStyle";
 import Home from "./pages/Home";
 import Equipeb from "./pages/Equipeb";
 import Equipec from "./pages/Equipec";
+import Equipej from "./pages/Equipej";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/equipeb" element={<Equipeb />} />
             <Route path="/equipec" element={<Equipec />} />
+            <Route path="/equipej" element={<Equipej />} />
           </Routes>
         </div>
       </Router>
