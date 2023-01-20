4adff42902e3b4dedfa316ddb870388461170a57
IgorSSSena <igorssena22@gmail.com>
2023-01-20 15:28:17 -0300

    Atualização da Equipe A
    Criação de uma div para fazer a atividade 2

--- src/pages/Equipea/index.jsx
+++ src/pages/Equipea/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Navbar from '../../components/navbarUser'
-import { H1 } from './styles';
 import { H2 } from './styles';
+import { DIV } from './styles';
+import { INPUT } from './styles';
 
 class Equipea extends React.Component {
  render(){
@@ -9,8 +10,19 @@ class Equipea extends React.Component {
     return (
         <>
             <Navbar/>
-            <H1>Teste</H1>
+            {/*primeiro teste */}
+            <br/>
             <H2>Alguém me salva</H2>
+             {/*SEGUNDO teste */}
+             <br/>
+             
+            <DIV>
+                <br/><br/><br/>
+                    <INPUT type="text" /> 
+                <br/><br/><br/><br/><br/>
+                    <INPUT type="text" />
+            </DIV>
+            
             
         </>
     )
