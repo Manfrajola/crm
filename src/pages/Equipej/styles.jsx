52e2b4d9fa41fa08d459245bfc3847422565958b
DiegoLabarewski <d.r.labarewski@gmail.com>
2023-01-20 17:35:16 -0300

    Update styles.jsx

--- src/pages/Equipej/styles.jsx
+++ src/pages/Equipej/styles.jsx
@@ -2,7 +2,7 @@ import styled from  "styled-components"
 
 export const H1 = styled.h1 `
     color: #4b4bc2;
-`
-export const Button = styled.h1 `
+`;
+export const Button = styled.button `
     
-`
+`;
