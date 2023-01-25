b78ce9b82a40a50d570cd27cc03aadec5c9113f2
Mateus <57973892+Mt-3108@users.noreply.github.com>
2023-01-25 10:06:47 -0300

    Atualização_Cards_EquipeD
    Atualização dos elementos Cards, utilizando mapping

--- src/pages/Equiped/index.jsx
+++ src/pages/Equiped/index.jsx
@@ -1,39 +1,84 @@
+
+// Imports Components
 import React,{ useState } from "react" ;
 import Navbar from "../../components/navbarUser";
+import Card from "../../components/Cards_EquipeD";
 
-import {H1} from "./styles";
-import { Span } from "./styles";
-//import{Div2} from "./styles";
+
+// Imports Style
+import {H1 , Div, Input} from "./styles";
+
 
 function Equiped() {
 
     const [text,setText] = useState()
 
+   
     const reflectedMessage = (e) => {
         setText(e.target.value);
     
       }
+  
+    const cards =[
+      {
+        id:1,
+        span1: "Habilitado",
+        span2: "Funciona",
+        text: "Primeiro Card"
+      },
+      {
+        id:2,
+        span1: "teste",
+        span2: "sempre",
+        text: "Segundo Card"
+      },
+      {
+        id:3,
+        span1: "Habilitado",
+        span2: "Funciona",
+        text: "Terceiro Card"
+      },{
+        id:4,
+        span1: "Teste 4 ",
+        span2: "Alguma coisa",
+        text: "Quarto card"
+      },
+    ]
     
-
-
+      
 
     return (
+      <>   
+            <Navbar />
+            <H1>Equipe D</H1>
 
-      <>
-        <Navbar />
-        <H1>Equiped</H1>
+
+            <Input type="text" id="textMessage" name="textMessage"
+              onChange={reflectedMessage} value={text} />
       
-    
-        <input type="text" id="textMessage" name="textMessage"
-          onChange={reflectedMessage} value={text}/>
-
-            <Span>{text}</Span>
+             <Input value={text}/>
+       
+       
+               <Div>
+                  {cards.map((item,i) =>(
+                        <Card key={i}
+                        id={item.id} span1={item.span1} 
+                        span2={item.span2} 
+                        text={item.text}/>))}
+              </Div>
+               
+              
+              
+              
+      
         
+           
+        </>)
         
-      </>
-    );
 }
+    
+      
+ 
 
-export default Equiped;
+  export default Equiped;
 
-//<Div2 className="divText2"></Div2>
--- src/pages/Equiped/styles.jsx
+++ src/pages/Equiped/styles.jsx
@@ -1,20 +1,33 @@
 import styled from "styled-components";
 
 export const H1 = styled.h1 `
-  color: red;
-  background-color: grey;
+padding-left: 1%;
+  color: orange;
+  background-color: #363636;  
+    
 `;
 
-export const Span = styled.span `
-    padding: 1px 58px;
-    
+export const Input = styled.input `
+    padding: 1px 50px;
+    margin-top : 15px;
+    margin-left: 15px;
+    margin-right: 15px;
     border: black solid 1px;
     border-radius: 5%;
+    max-width : 5%;
+
 `;
 
 
-/*export const Div2 = styled.divText2`
-    
-    padding-top: 3%;
-`;*/
+export const Div = styled.div `
+    background-color: #363636;
+    width:1030px;
+    height:900px;
+    margin-left: 30%;
+    border-radius: 10px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+`;
+
+
 
